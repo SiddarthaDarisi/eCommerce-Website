46fbeefb721dd283114b43d8ce61cd0b1d0ba8f1
harryvillian <37700453+HarshithBathula@users.noreply.github.com>
2024-01-21 23:44:21 -0500

    added Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const Navbar = () => {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Shop</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
