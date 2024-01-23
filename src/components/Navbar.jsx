462fe4e6f472a17d6c234f4e4f7e084053900505
SiddarthaDarisi <90901828+SiddarthaDarisi@users.noreply.github.com>
2024-01-22 23:01:37 -0500

    added colors

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,15 +2,13 @@ import React from "react";
 
 export const Navbar = () => {
   return (
-    <div>
-      <nav>
-        <ul>
-          <li>Home</li>
-          <li>Shop</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
-    </div>
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>Shop</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
   );
 };
