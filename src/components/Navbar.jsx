40fa4ddfb146581dc22f71cfabadb74127e1c40a
SiddarthaDarisi <90901828+SiddarthaDarisi@users.noreply.github.com>
2024-01-23 03:58:55 -0500

    navbar update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,50 @@
 import React from "react";
-
+import "./Navbar.css";
+import Logo from "../assets/Images/Logo.webp";
 export const Navbar = () => {
   return (
-    <nav>
-      <ul>
-        <li>Home</li>
-        <li>Shop</li>
-        <li>About</li>
-        <li>Contact</li>
-      </ul>
-    </nav>
+    <header className="header">
+      <a href="#">
+        <img className="logo" alt="furmiro logo" src={Logo} />
+      </a>
+      <nav className="main-nav">
+        <ul className="main-nav-list">
+          <li>
+            <a className="main-nav-link" href="#">
+              How it works
+            </a>
+          </li>
+          <li>
+            <a className="main-nav-link" href="#">
+              Meals
+            </a>
+          </li>
+
+          <li>
+            <a className="main-nav-link" href="#">
+              Pricing
+            </a>
+          </li>
+          <li>
+            <a className="main-nav-link nav-cta" href="#">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="#333"
+                className="nav-icons"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                />
+              </svg>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 };
