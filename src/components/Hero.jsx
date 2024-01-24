654ca2c7522dd4bf674aefc71ea9c18e43841e83
SiddarthaDarisi <90901828+SiddarthaDarisi@users.noreply.github.com>
2024-01-24 11:01:49 -0500

    small update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,17 +3,16 @@ export const Hero = () => {
   return (
     <div className="background">
       <section className="section-hero">
-        <div className="hero">
+        <div className="hero container">
           <div className="hero-text-box">
+            <p>New Arrival</p>
             <h1 className="heading-primary">Discover Our New Collection</h1>
             <p className="hero-description">
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Reprehenderit doloremque est vitae minima, dolore laboriosam
-              corporis sequi incidunt cupiditate vero. Voluptatum, inventore!
-              Quos, quo ab? Laudantium culpa voluptate ullam saepe?
+              Reprehenderit doloremque est vitae minima.
             </p>
             <a href="#" className="btn btn--full margin-right-sm">
-              Start eating well
+              Buy Now
             </a>
           </div>
         </div>
