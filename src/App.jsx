703d57593137f9c8fe097ae36cb9799183dfc326
SiddarthaDarisi <90901828+SiddarthaDarisi@users.noreply.github.com>
2024-01-25 08:31:42 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import { useState } from "react";
+
 import "./App.css";
 import { Navbar } from "./components/Navbar";
 import { Hero } from "./components/Hero";
 import { MainSection } from "./components/Mainsection";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Home } from "./components/Home";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
 function App() {
-  return (
-    <>
-      <Navbar />
-      <Hero />
-      <MainSection />
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import { Navbar } from "./Navbar";
+import { Hero } from "./Hero";
+import { MainSection } from "./Mainsection";
+
+export const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <MainSection />
+    </>
+  );
+};
